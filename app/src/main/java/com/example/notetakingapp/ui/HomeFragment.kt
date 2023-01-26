212c471d71f2fe59a55913a3b14709145fcab596
Techsultan <salamsultan09@gmail.com>
2023-01-26 15:43:53 +0100

    Cntinued work on View Model and Repository

--- app/src/main/java/com/example/notetakingapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/notetakingapp/ui/HomeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.notetakingapp.ui
 
+import android.app.Application
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.core.view.MenuProvider
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notetakingapp.MainActivity
@@ -14,13 +18,17 @@ import com.example.notetakingapp.R
 import com.example.notetakingapp.adapter.NoteAdapter
 import com.example.notetakingapp.databinding.FragmentNoteBinding
 import com.example.notetakingapp.model.Note
+import com.example.notetakingapp.viewmodel.NoteApplication
 import com.example.notetakingapp.viewmodel.NoteViewModel
 import com.example.notetakingapp.viewmodel.NoteViewModelFactory
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment(R.layout.fragment_note), MenuProvider {
 
-    private val noteViewModel: NoteViewModel by viewModels()
+    private val noteViewModel: NoteViewModel by activityViewModels{
+        val application: Application = NoteApplication()
+        NoteViewModelFactory((application as NoteApplication).repository)
+    }
 
     private var _binding: FragmentNoteBinding? = null
     private val binding get() = _binding!!
